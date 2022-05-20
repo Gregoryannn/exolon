e04a8c6fcbf4f9d9a29bcc04b24b98e9d6fb14f8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-20 15:56:22 +0100

    add AmmoHUD

--- src/resources/images.js
+++ src/resources/images.js
@@ -1,6 +1,35 @@
 define(function () {
 
     var images = [
+
+        // fonts
+        {
+            name: "font_white",
+            type: "image",
+            src: "images/font_white.gif",
+        },
+        {
+            name: "font_cyan",
+            type: "image",
+            src: "images/font_cyan.gif",
+        },
+        {
+            name: "font_green",
+            type: "image",
+            src: "images/font_green.gif",
+        },
+        {
+            name: "font_purple",
+            type: "image",
+            src: "images/font_purple.gif",
+        },
+        {
+            name: "font_yellow",
+            type: "image",
+            src: "images/font_yellow.gif",
+        },
+
+        // tiles
         {
             name: "metatiles",
             type: "image",
@@ -21,6 +50,8 @@ define(function () {
             type: "image",
             src: "images/gate.gif",
         },
+
+        // entities
         {
             name: "vitorc",
             type: "image",
@@ -35,6 +66,11 @@ define(function () {
             name: "grenade",
             type: "image",
             src: "images/grenade.gif",
+        },
+        {
+            name: "grenade_trace",
+            type: "image",
+            src: "images/grenade_trace.gif",
         },
         {
             name: "ship_fire",
--- src/util.js
+++ src/util.js
@@ -7,8 +7,17 @@ define(function () {
      */
     util.getRandomInt = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+   
+};
 
-    return util;
+util.strlpad = function (s, padString, length) {
+    var str = "" + s;
+    while (str.length < length) {
+        str = padString + str;
+    }
+    return str;
+};
+
+return util;
 
 });
