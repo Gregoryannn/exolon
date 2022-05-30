9f88246d3fd500efbd55316038d1741bd0872fea
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-30 20:08:23 +0100

    add ObstacleEntity

--- src/entities/CocoonEntity.js
+++ src/entities/CocoonEntity.js
@@ -1,32 +1,23 @@
 define(
     [
         "src/me",
-        "src/explosion",
+        "src/entities/ObstacleEntity",
     ],
     function (
         me,
-        explosion
+       ObstacleEntity
     ) {
 
-        var ObstacleEntity = me.ObjectEntity.extend({
+            var CocoonEntity = ObstacleEntity.extend({
 
-            init: function (x, y, settings) {
-                settings.image = "obstacle";
-                this.parent(x, y, settings);
+                init: function (x, y, settings) {
+                    settings.image = "cocoon";
+                    this.parent(x, y, settings);
 
-                this.collidable = true;
-            },
+                },
 
-            onCollision: function (res, obj) {
-                if (obj.name == "grenade") {
-                    explosion.create(this.pos.x + this.width / 2, this.pos.y + this.height / 2, this.z);
-                    me.game.remove(this);
-                    me.game.HUD.updateItemValue("points", 150);
-                }
-            },
+            });
+
+            return CocoonEntity;
 
         });
-
-        return ObstacleEntity;
-
-    });
--- src/entities/TurretEntity.js
+++ src/entities/TurretEntity.js
@@ -1,59 +1,43 @@
 define(
     [
         "src/me",
+        "src/entities/ObstacleEntity",
         "src/entities/TurretTubeEntity",
-        "src/entities/ExplosionParticleEntity",
     ],
     function (
         me,
-        TurretTubeEntity,
-        ExplosionParticleEntity
+        ObstacleEntity,
+        TurretTubeEntity
     ) {
 
-        var TurretEntity = me.ObjectEntity.extend({
+            var TurretEntity = ObstacleEntity.extend({
 
-            init: function (x, y, settings) {
-                settings.image = "turret_body";
-                settings.spritewidth = 64;
+                init: function (x, y, settings) {
+                    settings.image = "turret_body";
+                    settings.spritewidth = 64;
 
-                this.parent(x + 32, y, settings);
+                    this.parent(x + 32, y, settings);
 
-                this.updateColRect(8, 48, 4, 76);
+                    this.updateColRect(8, 48, 4, 76);
 
-                this.collidable = true;
-                this.tube = null;
+                    this.tube = null;
 
-                this.createTube(settings.z);
-            },
+                    this.createTube(settings.z);
+                },
 
-            createTube: function (z) {
-                this.tube = new TurretTubeEntity(this.pos.x - TurretTubeEntity.WIDTH, this.pos.y + 32, this);
-                me.game.add(this.tube, z);
-                me.game.sort();
-            },
+                createTube: function (z) {
+                    this.tube = new TurretTubeEntity(this.pos.x - TurretTubeEntity.WIDTH, this.pos.y + 32, this);
+                    me.game.add(this.tube, z);
+                    me.game.sort();
+                },
 
-            onCollision: function (res, obj) {
-                if (obj.name == "grenade") {
-                    this.createExplosion();
-                    me.game.remove(this.tube);
-                    me.game.remove(this);
-                    me.game.HUD.updateItemValue("points", 150);
-                }
-            },
+               
+                    onDestroyEvent: function () {
+                        me.game.remove(this.tube);
+                    },
 
-            createExplosion: function () {
-                for (var i = 0; i < TurretEntity.EXPLOSION_PARTICLES_COUNT; ++i) {
-                    var particle = new ExplosionParticleEntity(this.pos.x + this.width / 2, this.pos.y + this.height / 2);
-                    me.game.add(particle, this.z);
-                }
+                });
 
-                me.game.sort();
-            },
+            return TurretEntity;
 
         });
-
-        TurretEntity.EXPLOSION_PARTICLES_COUNT = 50;
-
-        return TurretEntity;
-
-    });
