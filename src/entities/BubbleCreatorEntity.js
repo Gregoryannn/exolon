66dd79802a7a6848a535eb077ec017a496ab7ac8
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-31 10:01:30 +0100

    add kamikaze creator

--- src/entities/BubbleCreatorEntity.js
+++ src/entities/BubbleCreatorEntity.js
@@ -1,44 +1,27 @@
 define(
     [
         "src/me",
-        "src/util",
+        "src/entities/KamikazeCreator",
         "src/entities/BubbleEntity",
     ],
     function (
         me,
-        util,
+        KamikazeCreator,
         BubbleEntity
     ) {
 
-        var BubbleCreatorEntity = Object.extend({
+            var BubbleCreatorEntity = KamikazeCreator.extend({
 
-            init: function () {
-                this.createTimer();
-            },
+              
+                delay: 3000,
+                    createKamikaze: function () {
+                        var vitorc = me.game.getEntityByName("vitorc")[0];
+                        var bubble = new BubbleEntity(512, vitorc.pos.y);
+                        me.game.add(bubble, vitorc.z);
+                        me.game.sort();
+                    },
 
-            update: function () {
-                return false;
-            },
+                });
 
-            createTimer: function () {
-                var self = this;
-                util.executeWithDelay(function () {
-                    self.createBubble();
-                    self.createTimer();
-                }, BubbleCreatorEntity.DELAY);
-            },
-
-            createBubble: function () {
-                var vitorc = me.game.getEntityByName("vitorc")[0];
-                var bubble = new BubbleEntity(512, vitorc.pos.y);
-                me.game.add(bubble, vitorc.z);
-                me.game.sort();
-            },
-
+         
         });
-
-        BubbleCreatorEntity.DELAY = 3000;
-
-        return BubbleCreatorEntity;
-
-    });
