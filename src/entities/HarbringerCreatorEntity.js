d087b1da1e153f8f598bf01ee12cfcd36792ca7e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-05-31 10:11:01 +0100

    can destroy bubble with blaster

---
+++ src/entities/HarbringerCreatorEntity.js
@@ -0,0 +1,23 @@
+define(
+    [
+        "src/entities/KamikazeCreatorEntity",
+        "src/entities/HarbringerEntity",
+    ],
+    function (
+        KamikazeCreatorEntity,
+        HarbringerEntity
+    ) {
+
+        var HarbringerCreatorEntity = KamikazeCreatorEntity.extend({
+
+            delay: 30000,
+
+            createSpecificKamikaze: function (x, y) {
+                return new HarbringerEntity(x, y);
+            },
+
+        });
+
+        return HarbringerCreatorEntity;
+
+    });
--- src/entities/KamikazeCreatorEntity.js
+++ src/entities/KamikazeCreatorEntity.js
@@ -8,41 +8,39 @@ define(
         util
     ) {
 
-        var KamikazeCreator = Object.extend({
-            var KamikazeCreatorEntity = Object.extend({
+        var KamikazeCreatorEntity = Object.extend({
 
-                delay: 5000,
+            delay: 5000,
 
-                init: function () {
-                    this.createTimer();
-                },
+            init: function () {
+                this.createTimer();
+            },
 
-                update: function () {
-                    return false;
-                },
+            update: function () {
+                return false;
+            },
 
-                createTimer: function () {
-                    var self = this;
-                    util.executeWithDelay(function () {
-                        self.createKamikaze();
-                        self.createTimer();
-                    }, this.delay);
-                },
+            createTimer: function () {
+                var self = this;
+                util.executeWithDelay(function () {
+                    self.createKamikaze();
+                    self.createTimer();
+                }, this.delay);
+            },
 
-                createKamikaze: function () {
-                    // should be overriden by child classes
-                    var vitorc = me.game.getEntityByName("vitorc")[0];
-                    var harbringer = this.createSpecificKamikaze(512, vitorc.pos.y);
-                    me.game.add(harbringer, vitorc.z - 1);
-                    me.game.sort();
-                },
+            createKamikaze: function () {
+                var vitorc = me.game.getEntityByName("vitorc")[0];
+                var harbringer = this.createSpecificKamikaze(512, vitorc.pos.y);
+                me.game.add(harbringer, vitorc.z - 1);
+                me.game.sort.defer();
+            },
 
-                createSpecificKamikaze: function () {
-                    // should be overriden by subclasses
-                },
-
-            });
-
-            return KamikazeCreatorEntity;
+            createSpecificKamikaze: function () {
+                // should be overriden by subclasses
+            },
 
         });
+
+        return KamikazeCreatorEntity;
+
+    });
