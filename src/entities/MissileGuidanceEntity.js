79370b7212d3a42f5b266b936dbb34286b6975f6
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-09 16:04:03 +0100

    destroy missile when guidance is destroyed

--- src/entities/MissileGuidanceEntity.js
+++ src/entities/MissileGuidanceEntity.js
@@ -2,20 +2,39 @@ define(
     [
         "src/me",
         "src/entities/ObstacleEntity",
+        "src/entities/MissileEntity",
     ],
     function (
         me,
-        ObstacleEntity
+        ObstacleEntity,
+        MissileEntity
     ) {
 
         var MissileGuidanceEntity = ObstacleEntity.extend({
 
             init: function (x, y, settings) {
+                this.vitorc = me.game.getEntityByName("vitorc")[0];
                 settings.image = "missile_guidance";
                 this.parent(x, y, settings);
             },
 
-        });
+            update: function () {
+                    if (me.gamestat.getItemValue("aliveMissilesCount") > 0 || !this.alive) {
+                        return false;
+                    }
+
+                    this.missile = new MissileEntity(this.vitorc);
+                    me.game.add(this.missile, this.vitorc.z);
+                    me.game.sort.defer();
+
+                    return false;
+                },
+
+                onDestroyEvent: function () {
+                    this.missile.explode();
+                },
+
+            });
 
         return MissileGuidanceEntity;
 
