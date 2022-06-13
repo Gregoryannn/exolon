40f38b26753f1eeac8850d4b76e7027a21a79196
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-13 14:12:12 +0100

    check collision with collision map for egg

--- src/entities/EggEntity.js
+++ src/entities/EggEntity.js
@@ -16,69 +16,82 @@ define(
 
             points: 50,
 
-              init: function (x, y, settings) {
-                    settings.image = "egg";
-                    settings.spritewidth = EggEntity.WIDTH;
-                    this.parent(x, y + EggEntity.HEIGHT, settings);
+            init: function (x, y, settings) {
+                settings.image = "egg";
+                settings.spritewidth = EggEntity.WIDTH;
+                this.parent(x, y + EggEntity.HEIGHT, settings);
 
-                   if (!settings.bounds) {
-                        settings.bounds = { x: x, y: y + EggEntity.HEIGHT, w: EggEntity.WIDTH, h: EggEntity.HEIGHT };
-                    }
-                    this.bounds = new me.Rect(new me.Vector2d(settings.bounds.x, settings.bounds.y), settings.bounds.w, settings.bounds.h);;
-                    this.increaseBounds = settings.increaseBounds ? settings.increaseBounds : false;
-                    this.animationspeed = 1;
-                    this.gravity = 0;
-                    this.collidable = true;
-                    this.isDestroyable = true;
-                    this.vel.x = util.getRandomArbitrary(-4, 2) + 1;
-                    this.vel.y = util.getRandomArbitrary(-1.5, 1) + 0.5;
-                },
+                if (!settings.bounds) {
+                    settings.bounds = { x: x, y: y + EggEntity.HEIGHT, w: EggEntity.WIDTH, h: EggEntity.HEIGHT };
+                }
+                this.bounds = new me.Rect(new me.Vector2d(settings.bounds.x, settings.bounds.y), settings.bounds.w, settings.bounds.h);;
+                this.increaseBounds = settings.increaseBounds ? settings.increaseBounds : false;
+                this.animationspeed = 1;
+                this.gravity = 0;
+                this.collidable = true;
+                this.isDestroyable = true;
+                this.vel.x = util.getRandomArbitrary(-4, 2) + 1;
+                this.vel.y = util.getRandomArbitrary(-1.5, 1) + 0.5;
+            },
 
-                updateMovement: function () {
-                    this.pos.x += this.vel.x;
+            updateMovement: function () {
+                this.prevX = this.pos.x;
+                this.prevY = this.pos.y;
 
-                    if (this.right > this.bounds.right ||
-                        this.left < this.bounds.left) {
+                this.pos.add(this.vel);
+                this.pos.y += util.getRandomArbitrary(0.1, 0.5) * Math.sin(this.pos.x / 4);
 
-                        if (this.increaseBounds) {
-                            if (this.right > this.bounds.right) {
-                                this.bounds.width += EggEntity.BOUNDS_INC;
-                            }
-                            else if (this.left < this.bounds.left) {
-                                this.bounds.pos.x -= EggEntity.BOUNDS_INC;
-                            }
+                if (this.right > this.bounds.right ||
+                    this.left < this.bounds.left) {
+
+                    if (this.increaseBounds) {
+                        if (this.right > this.bounds.right) {
+                            this.bounds.width += EggEntity.BOUNDS_INC;
                         }
-
-                        this.pos.x -= this.vel.x;
-                        this.vel.x = -this.vel.x;
+                        else if (this.left < this.bounds.left) {
+                            this.bounds.pos.x -= EggEntity.BOUNDS_INC;
+                        }
                     }
 
-                    this.prevY = this.pos.y;
-                    this.pos.y += this.vel.y;
-                    this.pos.y += util.getRandomArbitrary(0.1, 0.5) * Math.sin(this.pos.x / 4);
+                    this.pos.x = this.prevX;
+                    this.vel.x = -this.vel.x;
+                }
 
-                    if (this.bottom > this.bounds.bottom ||
-                        this.top < this.bounds.top) {
+             if (this.bottom > this.bounds.bottom ||
+                    this.top < this.bounds.top) {
 
-                        if (this.increaseBounds) {
-                            if (this.bottom > this.bounds.bottom) {
-                                this.bounds.height += EggEntity.BOUNDS_INC;
-                            }
-                            else if (this.top < this.bounds.top) {
-                                this.bounds.pos.y -= EggEntity.BOUNDS_INC;
-                            }
+                    if (this.increaseBounds) {
+                        if (this.bottom > this.bounds.bottom) {
+                            this.bounds.height += EggEntity.BOUNDS_INC;
                         }
+                        else if (this.top < this.bounds.top) {
+                            this.bounds.pos.y -= EggEntity.BOUNDS_INC;
+                        }
+                    }
 
-                        this.pos.y = this.prevY;
+                    this.pos.y = this.prevY;
+                    this.vel.y = -this.vel.y;
+                }
+
+                var res = this.collisionMap.checkCollision(this.collisionBox, this.vel);
+
+                if (res) {
+                    this.pos.sub(res);
+
+                    if (res.y) {
                         this.vel.y = -this.vel.y;
                     }
-                },
+                    if (res.x) {
+                        this.vel.x = -this.vel.x;
+                    }
+                }
+            },
 
-                createSpecificExplosion: function (x, y) {
-                    return new BlasterExplosion(x, y);
-                },
+            createSpecificExplosion: function (x, y) {
+                return new BlasterExplosion(x, y);
+            },
 
-            });
+        });
 
         EggEntity.WIDTH = 16;
         EggEntity.HEIGHT = 16;
