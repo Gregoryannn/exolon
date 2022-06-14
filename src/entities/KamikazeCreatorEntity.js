185de193c21fa2cf01da79477eb21cd138806704
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-14 13:15:26 +0100

    add L01S25

--- src/entities/KamikazeCreatorEntity.js
+++ src/entities/KamikazeCreatorEntity.js
@@ -20,6 +20,9 @@ define(
                 if (this.vitorc == null) {
                     this.vitorc = me.game.getEntityByName("vitorc")[0];
                 }
+                if (this.vitorc.isCurrentAnimation("die")) {
+                    this.timer = 0;
+                }
                 this.timer += me.timer.tick / me.sys.fps;
                 if (this.timer > this.delay) {
                     this.timer = 0;
@@ -29,25 +32,26 @@ define(
             },
 
             createKamikaze: function () {
-                if (this.vitorc.isCurrentAnimation("die") || !this.shouldCreate()) {
-                    return;
-                }
-                var kamikaze = this.createSpecificKamikaze(512, this.vitorc.pos.y + 2);
-                me.game.add(kamikaze, this.vitorc.z);
-                me.game.sort.defer();
-            },
+                    if (!this.shouldCreate()) {
+                        return;
+                    }
+                    var kamikaze = this.createSpecificKamikaze(512, this.vitorc.pos.y + 2);
+                    me.game.add(kamikaze, this.vitorc.z);
+                    me.game.sort.defer();
+                },
 
-            createSpecificKamikaze: function () {
-                // should be overriden by subclasses
-            },
+                createSpecificKamikaze: function () {
+                    // should be overriden by subclasses
+                },
 
-            shouldCreate: function () {
-                if (this.vitorc.pos.x > 340) {
-                    return false;
-                }                return true;
-            },
+                shouldCreate: function () {
+                    if (this.vitorc.pos.x > 340) {
+                        return false;
+                    }
+                    return true;
+                },
 
-        });
+            });
 
         return KamikazeCreatorEntity;
 
