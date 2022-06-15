da9203e66d8aab42b56d68f9138bf4b522769dfd
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-15 15:51:08 +0100

    award points on level exit

--- src/entities/AwardPointsEntity.js
+++ src/entities/AwardPointsEntity.js
@@ -8,32 +8,32 @@ define(
 
         var AwardPointsEntity = Object.extend({
 
-            init: function (points) {
-                this.points = points;
-                this.step = 25;
-                this.timer = 0;
-                this.delay = 0.04;
-            },
-
-            update: function () {
-                this.timer += me.timer.tick / me.sys.fps;
-                if (this.timer > this.delay) {
+                init: function (points, step) {
+                    this.points = points;
+                    this.step = step !== undefined ? step : 25;
                     this.timer = 0;
-                    this.award();
-                }
-                return false;
-            },
+                    this.delay = 0.04;
+                },
 
-            award: function () {
-                this.points -= this.step;
-                me.game.HUD.updateItemValue("points", this.step);
+                update: function () {
+                    this.timer += me.timer.tick / me.sys.fps;
+                    if (this.timer > this.delay) {
+                        this.timer = 0;
+                        this.award();
+                    }
+                    return false;
+                },
 
-                if (this.points <= 0) {
-                    me.game.remove(this);
-                }
-            },
+                award: function () {
+                    this.points -= this.step;
+                    me.game.HUD.updateItemValue("points", this.step);
 
-        });
+                    if (this.points <= 0) {
+                        me.game.remove(this);
+                    }
+                },
+
+            });
 
         return AwardPointsEntity;
 
