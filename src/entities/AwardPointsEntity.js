2e20f06584f3e5c11cf6a44cfd96eef6f6307f99
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-06-17 14:31:18 +0100

    award points: handle case when player left the screen before all poin…

--- src/entities/AwardPointsEntity.js
+++ src/entities/AwardPointsEntity.js
@@ -1,50 +1,54 @@
 define(
     [
-        define(
-            [
-                "src/me",
-                "src/util",
-            ],
-            function (
-                me
-    me,
-                util
-            ) {
+        "src/me",
+        "src/util",
+    ],
+    function (
+        me,
+        util
+    ) {
 
-                var AwardPointsEntity = Object.extend({
+        var AwardPointsEntity = Object.extend({
 
-                    init: function (points, step) {
-                        this.points = points;
-                        this.step = step !== undefined ? step : 25;
-                        this.timer = 0;
-                        this.delay = 0.04;
-                        this.onComplete = null;
-                    },
+            init: function (points, step) {
+                this.points = points;
+                this.step = step !== undefined ? step : 25;
+                this.timer = 0;
+                this.delay = 0.04;
+                this.onComplete = null;
+            },
 
-                    update: function () {
-                        this.timer += me.timer.tick / me.sys.fps;
-                        if (this.timer > this.delay) {
-                            this.timer = 0;
-                            this.award();
-                        }
-                        return false;
-                    },
+            update: function () {
+                this.timer += me.timer.tick / me.sys.fps;
+                if (this.timer > this.delay) {
+                    this.timer = 0;
+                    this.award();
+                }
+                return false;
+            },
 
-                    award: function () {
-                        this.points -= this.step;
-                        me.game.HUD.updateItemValue("points", this.step);
-                        util.updatePoints(this.step);
+            award: function () {
+                this.points -= this.step;
+                util.updatePoints(this.step);
 
-                        if (this.points <= 0) {
-                            if (this.onComplete) {
-                                this.onComplete();
-                            }
-                            me.game.remove(this);
-                        }
-                    },
+                if (this.points <= 0) {
+                    if (this.onComplete) {
+                        this.onComplete();
+                    }
+                    me.game.remove(this);
+                }
+            },
 
-                });
+            destroy: function () {
+                // If player left the screen before all points were added, add remaining
+                // points.
+                if (this.points > 0) {
+                    util.updatePoints(this.points);
+                }
+            },
 
-                return AwardPointsEntity;
+        });
 
-            });
+        return AwardPointsEntity;
+
+    });
